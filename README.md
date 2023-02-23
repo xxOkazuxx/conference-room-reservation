b358f9822267f5dfbb26749d8a85d86f31358394
Kazuki Ochiai <kazu910739@gmail.com>
2023-02-23 22:13:33 +0900

    Model,APIテスト画面作成

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # conference-room-reservation
 This is the conference room reservation Web application with FastAPI.
+
+## How to run streamlit server
+streamlit run app.py
+
+## How to activate FastAPI
+uvicorn main:app --reload
---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import random
+import requests
+import json
+
+st.title('APIテスト画面(ユーザー)')
+
+with st.form(key='user'):
+    user_id: int = random.randint(0,10)
+    username: str = st.text_input('ユーザー名', max_chars=12)
+    data = {
+        'user_id': user_id,
+        'username': username
+    }
+    submit_button = st.form_submit_button(label='リクエスト送信')
+    
+    if submit_button:
+       st.write('## 送信データ')
+       st.json(data)
+       st.write('## レスポンス結果')
+       url = 'http://127.0.0.1:8000/users'
+       res = requests.post(
+           url,
+           data=json.dumps(data)
+       )
+       st.write(res.status_code)
+       st.json(res.json())
