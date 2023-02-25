d1f8006520038394b6e5ec0819f4ea5ca343ddea
Kazuki Ochiai <kazu910739@gmail.com>
2023-02-26 00:37:55 +0900

    ユーザー登録画面　作成

--- app.py
+++ app.py
@@ -6,26 +6,25 @@ import datetime
 
 page = st.sidebar.selectbox('Choose your page', ['users', 'rooms', 'bookings'])
 if page == 'users':
-    st.title('APIテスト画面(ユーザー)')
+    st.title('ユーザー登録画面')
     with st.form(key='user'):
-        user_id: int = random.randint(0,10)
+        # user_id: int = random.randint(0,10)
         username: str = st.text_input('ユーザー名', max_chars=12)
         data = {
-            'user_id': user_id,
+            # 'user_id': user_id,
             'username': username
         }
-        submit_button = st.form_submit_button(label='リクエスト送信')
+        submit_button = st.form_submit_button(label='ユーザー登録')
 
     if submit_button:
-        st.write('## 送信データ')
-        st.json(data)
         st.write('## レスポンス結果')
         url = 'http://127.0.0.1:8000/users'
         res = requests.post(
             url,
             data=json.dumps(data)
         )
-        st.write(res.status_code)
+        if res.status_code == 200:
+            st.success('ユーザー登録完了')
         st.json(res.json())
 
 elif page == 'rooms':
--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,25 +1,32 @@
 import datetime
 from pydantic import BaseModel, Field
 
-class Booking(BaseModel):
-    booking_id: int
+class BookingiCreate(BaseModel):
     user_id: int
     room_id: int
     booked_num: int
     start_datetime: datetime.datetime
     end_datetime: datetime.datetime
 
+class Booking(BookingiCreate):
+    booking_id: int
+
     class Config:
         orm_mode = True
 
-class User(BaseModel):
-    user_id: int
+class UserCreate(BaseModel):
     username: str = Field(max_length=12)
 
+class User(UserCreate):
+    user_id: int
     class Config:
         orm_mode = True
 
-class Room(BaseModel):
+class RoomCreate(BaseModel):
+    room_name: str = Field(max_length=12)
+    capacity: int
+
+class Room(RoomCreate):
     room_id: int
     room_name: str = Field(max_length=12)
     capacity: int
