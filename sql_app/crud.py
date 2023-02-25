74699518a7274d6484dc18f0715713e916f2457e
Kazuki Ochiai <kazu910739@gmail.com>
2023-02-25 17:43:44 +0900

    データ登録、更新処理作成

---
+++ sql_app/crud.py
@@ -0,0 +1,47 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+
+# ユーザー一覧取得
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+# 会議室一覧取得
+def get_rooms(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Room).offset(skip).limit(limit).all()
+
+# 予約一覧取得
+def get_bookings(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Booking).offset(skip).limit(limit).all()
+
+
+# ユーザー登録
+def create_user(db: Session, user: schemas.User):
+    db_user = models.User(username=user.username)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+# 会議室登録
+def create_room(db: Session, room: schemas.Room):
+    db_room = models.Room(room_name=room.room_name, capacity=room.capacity)
+    db.add(db_room)
+    db.commit()
+    db.refresh(db_room)
+    return db_room
+
+# 予約登録
+def create_booking(db: Session, booking: schemas.Booking):
+    db_booking = models.Booking(
+        user_id = booking.user_id,
+        room_id = booking.room_id,
+        booked_num = booking.booked_num,
+        start_time = booking.start_time,
+        end_time = booking.end_time
+    )
+    db.add(db_booking)
+    db.commit()
+    db.refresh(db_booking)
+    return db_booking
+
--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,5 +1,4 @@
 import datetime
-from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
 class Booking(BaseModel):
@@ -10,29 +9,20 @@ class Booking(BaseModel):
     start_datetime: datetime.datetime
     end_datetime: datetime.datetime
 
+    class Config:
+        orm_mode = True
+
 class User(BaseModel):
     user_id: int
     username: str = Field(max_length=12)
+
+    class Config:
+        orm_mode = True
 
 class Room(BaseModel):
     room_id: int
     room_name: str = Field(max_length=12)
     capacity: int
 
-app = FastAPI()
-
-@app.get("/")
-async def index():
-    return {"message": "Success"}
-
-@app.post("/users")
-async def users(users: User):
-    return {"users": users}
-
-@app.post("/rooms")
-async def rooms(rooms: Room):
-    return {"rooms": rooms}
-
-@app.post("/bookings")
-async def bookings(bookings: Booking):
-    return {"booking": bookings}
+    class Config:
+        orm_mode = True
